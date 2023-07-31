5eceb1e7b0d35cc87f2a46cf6948ed724fdb91a6
Caliche Orozco <caliche2da1@gmail.com>
2023-07-30 21:02:43 -0600

    update: added auto

--- src/constants.ts
+++ src/constants.ts
@@ -1,34 +1,34 @@
 export const SUPPORTED_LANGUAGES = {
-  en: 'English',
-  es: 'Spanish',
-  fr: 'French',
-  de: 'German',
-  it: 'Italian',
-  pt: 'Portuguese',
-  nl: 'Dutch',
-  pl: 'Polish',
-  ru: 'Russian',
-  ja: 'Japanese',
-  zh: 'Chinese',
-  ar: 'Arabic',
-  tr: 'Turkish'
-}
+  en: "English",
+  es: "Spanish",
+  fr: "French",
+  de: "German",
+  it: "Italian",
+  pt: "Portuguese",
+  nl: "Dutch",
+  pl: "Polish",
+  ru: "Russian",
+  ja: "Japanese",
+  zh: "Chinese",
+  ar: "Arabic",
+  tr: "Turkish",
+};
 
 export const VOICES_FOR_LANGUAGES = {
-  en: 'en-US',
-  es: 'es-MX',
-  fr: 'fr-FR',
-  de: 'de-DE',
-  it: 'it-IT',
-  pt: 'pt-BR',
-  nl: 'nl-NL',
-  pl: 'pl-PL',
-  ru: 'ru-RU',
-  ja: 'ja-JP',
-  zh: 'zh-CN',
-  ar: 'ar-SA',
-  tr: 'tr-TR'
-}
+  auto: "auto",
+  en: "en-US",
+  es: "es-MX",
+  fr: "fr-FR",
+  de: "de-DE",
+  it: "it-IT",
+  pt: "pt-BR",
+  nl: "nl-NL",
+  pl: "pl-PL",
+  ru: "ru-RU",
+  ja: "ja-JP",
+  zh: "zh-CN",
+  ar: "ar-SA",
+  tr: "tr-TR",
+};
 
-
-export const AUTO_LANGUAGES = 'auto'
+export const AUTO_LANGUAGES = "auto";
